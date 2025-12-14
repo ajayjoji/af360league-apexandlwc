05656170aed910d9becd0b566c0fe6ef1eac47aa
ajayjoji <ajayjoji@gmail.com>
2025-12-14 19:39:09 +0530

    test

--- force-app/main/default/lwc/promotionWizardStep1/promotionWizardStep1.js
+++ force-app/main/default/lwc/promotionWizardStep1/promotionWizardStep1.js
@@ -10,6 +10,7 @@ export default class PromotionWizardStep1 extends LightningElement {
 
     connectedCallback(){
         this.promotionName = this.promotionState?.value?.promotionName;
+        alert('Loaded promotion name: ' + this.promotionName);
     }
 
     handleChange(event) {
